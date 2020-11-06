c4e43618bbcf7fffa743372b0918c0c1b3c5c4fd
Lervin Urena <l.urena@nexus.com.do>
2020-11-06 17:39:21 -0400

    dep

---
+++ Pods/Target Support Files/Pods-NextJob-NextJobUITests/Pods-NextJob-NextJobUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NextJob_NextJobUITests : NSObject
+@end
+@implementation PodsDummy_Pods_NextJob_NextJobUITests
+@end
---
+++ Pods/Target Support Files/Pods-NextJob/Pods-NextJob-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NextJob : NSObject
+@end
+@implementation PodsDummy_Pods_NextJob
+@end
